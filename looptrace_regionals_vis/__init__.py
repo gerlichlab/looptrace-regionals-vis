715ab168d7963755051c1464926cdfb0cf604749
Vince Reuter <vince.reuter@gmail.com>
2024-04-29 14:34:21 +0200

    bump version to 0.2dev

--- looptrace_regionals_vis/__init__.py
+++ looptrace_regionals_vis/__init__.py
@@ -10,7 +10,7 @@ from pathlib import Path
 
 from numpydoc_decorator import doc  # type: ignore[import-untyped]
 
-__version__ = "0.1.0"
+__version__ = "0.2dev"
 
 _PACKAGE_NAME = package = Path(__file__).parent.name
 
