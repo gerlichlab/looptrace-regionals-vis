27a7bfb22bebbeb3d78fa4ad38575cf0a47060bc
Vince Reuter <vince.reuter@gmail.com>
2024-10-18 13:44:27 +0200

    try simplifying the yellow color specification to see if napari will pick it up

--- looptrace_regionals_vis/colors.py
+++ looptrace_regionals_vis/colors.py
@@ -6,4 +6,4 @@ IBM_BLUE = "#648FFF"  # for merge contributors
 IBM_PURPLE = "#785EF0"  # for proximity discards
 IBM_PINK = "#DC267F"  # for accepted singletons
 IBM_ORANGE = "#FE6100"  # for non-nuclear
-IBM_YELLOW = "FFB000"  # for accepted mergers
+IBM_YELLOW = "yellow" #"FFB000"  # for accepted mergers
