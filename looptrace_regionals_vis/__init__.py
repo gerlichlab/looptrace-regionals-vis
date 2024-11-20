5c87fb163e51f38771185a7702d45caa405f9f82
Vince Reuter <vince.reuter@gmail.com>
2024-11-20 22:18:44 +0100

    update version number and docs for v0.3 release

--- looptrace_regionals_vis/__init__.py
+++ looptrace_regionals_vis/__init__.py
@@ -10,7 +10,7 @@ from pathlib import Path
 
 from numpydoc_decorator import doc  # type: ignore[import-untyped]
 
-__version__ = "0.3dev"
+__version__ = "0.3"
 
 _PACKAGE_NAME = package = Path(__file__).parent.name
 
