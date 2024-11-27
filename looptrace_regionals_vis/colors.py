928ec013be9196728bc63d3700c8829e5167367c
Vince Reuter <vince.reuter@gmail.com>
2024-11-27 21:23:45 +0100

    higher lower bound on ruff to sync w/ Github

--- looptrace_regionals_vis/colors.py
+++ looptrace_regionals_vis/colors.py
@@ -1,6 +1,6 @@
 """Colors used for data visualisation"""
 
-__all__ = ["IBM_BLUE", "IBM_PURPLE", "IBM_PINK", "IBM_ORANGE", "IBM_YELLOW"]
+__all__ = ["IBM_BLUE", "IBM_ORANGE", "IBM_PINK", "IBM_PURPLE", "IBM_YELLOW"]
 
 IBM_BLUE = "#648FFF"  # for merge contributors
 IBM_PURPLE = "#785EF0"  # for proximity discards
